48feff593eb902e7c19e4b7fce8540d66f6801da
Joshua Ide <Josh@192-168-1-100.tpgi.com.au>
2016-01-05 18:45:33 +1100

    Fixed landscape issue

--- MyLittleMonster/DragImg.swift
+++ MyLittleMonster/DragImg.swift
@@ -35,7 +35,7 @@ class DragImg: UIImageView {
     
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
         if let touch = touches.first, let target = dropTarget {
-            let position = touch.locationInView(self.superview)
+            let position = touch.locationInView(self.superview?.superview)
             
             if CGRectContainsPoint(target.frame, position) {
                 NSNotificationCenter.defaultCenter().postNotification(NSNotification(name: "onTargetDropped", object: nil))
